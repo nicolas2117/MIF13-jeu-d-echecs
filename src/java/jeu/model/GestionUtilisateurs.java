cfa8db74f8800903f8c102424aebedb6cc89e140
nicolas <nicolas2117@gmail.com>
2011-11-07 23:48:31 +0100

    gna

--- src/java/jeu/model/GestionUtilisateurs.java
+++ src/java/jeu/model/GestionUtilisateurs.java
@@ -27,6 +27,15 @@ public class GestionUtilisateurs {
         return true;
     }
     
+    public Utilisateur getUtilisateurParPseudo(String pseudo) {
+        for(int i = 0; i < utilisateurs.size(); i++) {
+            if (utilisateurs.get(i).getPseudo().equals(pseudo)) {
+                return utilisateurs.get(i);
+            }
+        }
+        return null;      
+    }
+    
     public void ajouterUtilisateur(Utilisateur utilisateur) {
         utilisateurs.add(utilisateur);
     }
